23d4f023af58c08372c71bf34da791711c91827a
Loris Giann <loris.giann@live.it>
2022-07-11 17:23:44 +0200

    test qak wasteservice

--- sprint1/QakSprint1/demo0.pl
+++ sprint1/QakSprint1/demo0.pl
@@ -2,6 +2,7 @@
 % demo0 description   
 %====================================================================================
 context(ctxrpi, "localhost",  "TCP", "8095").
- qactor( waste_service, ctxrpi, "it.unibo.waste_service.Waste_service").
-  qactor( transporttrolley, ctxrpi, "it.unibo.transporttrolley.Transporttrolley").
+ qactor( wasteservice, ctxrpi, "it.unibo.wasteservice.Wasteservice").
   qactor( led, ctxrpi, "it.unibo.led.Led").
+  qactor( testwasteservice, ctxrpi, "it.unibo.testwasteservice.Testwasteservice").
+  qactor( transporttrolley, ctxrpi, "it.unibo.transporttrolley.Transporttrolley").
--- sprint1/QakSprint1/src/it/unibo/waste_service/Waste_service.kt
+++ sprint1/QakSprint1/src/it/unibo/waste_service/Waste_service.kt
@@ -14,22 +14,24 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 		return "wait"
 	}
 	override fun getBody() : (ActorBasicFsm.() -> Unit){
-		 fun checkdepositpossible(MATERIAL:String,LOAD:String) : Boolean {
-		 				return (MATERIAL=="plastic" && LOAD.toInt()+contPB<=MAXPB) 
-		 				 || (MATERIAL=="glass" && LOAD.toInt()+contGB<=MAXGB);
-		 			} 
-		 			
-				const val MAXPB = 10
-				const val MAXGB = 10
-				lateinit var contPB : String 
-				lateinit var contGB : String
+		 
+				val MAXPB = 10
+				val MAXGB = 10
+				var contPB = 0
+				var contGB = 0
 				lateinit var Material  : String
 				lateinit var TruckLoad : String
-				lateint var RES : String
+				lateinit var RES : String
+				
+				fun checkdepositpossible(MATERIAL:String,LOAD:String) : Boolean {
+		 				return (MATERIAL=="plastic" && LOAD.toInt()+contPB<=MAXPB) 
+		 				 || (MATERIAL=="glass" && LOAD.toInt()+contGB<=MAXGB);
+		 		}
 		return { //this:ActionBasciFsm
 				state("wait") { //this:State
 					action { //it:State
 						discardMessages = false
+						println("Waiting for requests")
 					}
 					 transition(edgeName="t00",targetState="handle_req",cond=whenRequest("depositrequest"))
 				}	 
@@ -41,15 +43,18 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 								
 												 Material 	= payloadArg(0) ;
 												 TruckLoad 	= payloadArg(1) ;
+								println("arrived $TruckLoad Kg of $Material")
 								if(  checkdepositpossible( Material, TruckLoad )  
-								 ){request("move", "move("INDOOR")" ,"transporttrolley" )  
+								 ){request("move", "move(INDOOR)" ,"transporttrolley" )  
 								}
 								else
 								 {answer("depositrequest", "loadrejected", "loadrejected($Material,$TruckLoad)"   )  
+								 forward("noMsg", "noMsg(_)" ,"waste_service" ) 
 								 }
 						}
 					}
 					 transition(edgeName="t01",targetState="handle_move_indoor",cond=whenReply("moveanswer"))
+					transition(edgeName="t02",targetState="wait",cond=whenDispatch("noMsg"))
 				}	 
 				state("handle_move_indoor") { //this:State
 					action { //it:State
@@ -60,13 +65,12 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 								if(  RES == "OK"  
 								 ){request("pickup", "pickup(_)" ,"transporttrolley" )  
 								}
-								forward("noMsg", "noMsg(_)" ,"waste_service" ) 
 						}
 					}
-					 transition(edgeName="t02",targetState="handle_pickup_answer",cond=whenDispatchGuarded("noMsg",{ RES=="OK"  
-					}))
-					transition(edgeName="t03",targetState="error",cond=whenDispatchGuarded("noMsg",{ RES!="OK"  
-					}))
+					 transition( edgeName="goto",targetState="handle_pickup_answer", cond=doswitchGuarded({ RES=="OK"  
+					}) )
+					transition( edgeName="goto",targetState="error", cond=doswitchGuarded({! ( RES=="OK"  
+					) }) )
 				}	 
 				state("handle_pickup_answer") { //this:State
 					action { //it:State
@@ -78,13 +82,12 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 								 ){request("move", "move($Material)" ,"transporttrolley" )  
 								answer("depositrequest", "loadaccept", "loadaccept($Material,$TruckLoad)"   )  
 								}
-								forward("noMsg", "noMsg(_)" ,"waste_service" ) 
 						}
 					}
-					 transition(edgeName="t04",targetState="handle_move_container",cond=whenDispatchGuarded("noMsg",{ RES=="OK"  
-					}))
-					transition(edgeName="t05",targetState="error",cond=whenDispatchGuarded("noMsg",{ RES!="OK"  
-					}))
+					 transition( edgeName="goto",targetState="handle_move_container", cond=doswitchGuarded({ RES=="OK"  
+					}) )
+					transition( edgeName="goto",targetState="error", cond=doswitchGuarded({! ( RES=="OK"  
+					) }) )
 				}	 
 				state("handle_move_container") { //this:State
 					action { //it:State
@@ -95,13 +98,12 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 								if(  RES == "OK"  
 								 ){request("dropout", "dropout(_)" ,"transporttrolley" )  
 								}
-								forward("noMsg", "noMsg(_)" ,"waste_service" ) 
 						}
 					}
-					 transition(edgeName="t06",targetState="handle_dropout_answer",cond=whenDispatchGuarded("noMsg",{ RES=="OK"  
-					}))
-					transition(edgeName="t07",targetState="error",cond=whenDispatchGuarded("noMsg",{ RES!="OK"  
-					}))
+					 transition( edgeName="goto",targetState="handle_dropout_answer", cond=doswitchGuarded({ RES=="OK"  
+					}) )
+					transition( edgeName="goto",targetState="error", cond=doswitchGuarded({! ( RES=="OK"  
+					) }) )
 				}	 
 				state("handle_dropout_answer") { //this:State
 					action { //it:State
@@ -116,10 +118,10 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 						stateTimer = TimerActor("timer_handle_dropout_answer", 
 							scope, context!!, "local_tout_waste_service_handle_dropout_answer", 10.toLong() )
 					}
-					 transition(edgeName="t08",targetState="handle_move_home",cond=whenTimeout("local_tout_waste_service_handle_dropout_answer"))   
-					transition(edgeName="t09",targetState="error",cond=whenDispatchGuarded("noMsg",{ RES!="OK"  
+					 transition(edgeName="t03",targetState="handle_move_home",cond=whenTimeout("local_tout_waste_service_handle_dropout_answer"))   
+					transition(edgeName="t04",targetState="error",cond=whenDispatchGuarded("noMsg",{ RES!="OK"  
 					}))
-					transition(edgeName="t010",targetState="handle_new_request",cond=whenRequest("depositrequest"))
+					transition(edgeName="t05",targetState="handle_new_request",cond=whenRequest("depositrequest"))
 				}	 
 				state("handle_new_request") { //this:State
 					action { //it:State
@@ -130,7 +132,7 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 												 Material 	= payloadArg(0) ;
 												 TruckLoad 	= payloadArg(1) ;
 								if(  checkdepositpossible( Material, TruckLoad )  
-								 ){request("move", "move("INDOOR")" ,"transporttrolley" )  
+								 ){request("move", "move(INDOOR)" ,"transporttrolley" )  
 								}
 								else
 								 {answer("depositrequest", "loadrejected", "loadrejected($Material,$TruckLoad)"   )  
@@ -138,12 +140,12 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 								 }
 						}
 					}
-					 transition(edgeName="t011",targetState="handle_move_indoor",cond=whenReply("moveanswer"))
-					transition(edgeName="t012",targetState="handle_move_home",cond=whenDispatch("noMsg"))
+					 transition(edgeName="t06",targetState="handle_move_indoor",cond=whenReply("moveanswer"))
+					transition(edgeName="t07",targetState="handle_move_home",cond=whenDispatch("noMsg"))
 				}	 
 				state("handle_move_home") { //this:State
 					action { //it:State
-						request("move", "move("HOME")" ,"transporttrolley" )  
+						request("move", "move(HOME)" ,"transporttrolley" )  
 					}
 					 transition( edgeName="goto",targetState="handle_move_home_answer", cond=doswitch() )
 				}	 
@@ -161,10 +163,10 @@ class Waste_service ( name: String, scope: CoroutineScope  ) : ActorBasicFsm( na
 								 }
 						}
 					}
-					 transition(edgeName="t013",targetState="handle_req",cond=whenRequest("depositrequest"))
-					transition(edgeName="t014",targetState="error",cond=whenDispatchGuarded("noMsg",{ RES!="OK"  
+					 transition(edgeName="t08",targetState="handle_req",cond=whenRequest("depositrequest"))
+					transition(edgeName="t09",targetState="error",cond=whenDispatchGuarded("noMsg",{ RES!="OK"  
 					}))
-					transition(edgeName="t015",targetState="wait",cond=whenReply("moveanswer"))
+					transition(edgeName="t010",targetState="wait",cond=whenReply("moveanswer"))
 				}	 
 				state("error") { //this:State
 					action { //it:State
