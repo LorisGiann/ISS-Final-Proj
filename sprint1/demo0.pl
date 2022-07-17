d4ba0032fc3b3f1bbf0c46db110c05e0e8ef0917
Loris Giann <loris.giann@live.it>
2022-07-17 09:07:10 +0200

    cleaning directories

--- sprint1/demo0.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% demo0 description   
-%====================================================================================
-context(ctxrpi, "localhost",  "TCP", "8095").
- qactor( wasteservice, ctxrpi, "it.unibo.wasteservice.Wasteservice").
-  qactor( testwasteservice, ctxrpi, "it.unibo.testwasteservice.Testwasteservice").
-  qactor( transporttrolley, ctxrpi, "it.unibo.transporttrolley.Transporttrolley").
