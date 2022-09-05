ffb8057076e6c110c57920964fc50ec0dca0d92b
ilmada99 <davgui99@gmail.com>
2022-09-05 15:18:28 +0200

    First version sprint3.html

--- sprint3/WebRobotKt/src/main/resources/static/wsminimal.js
+++ sprint3/WebRobotKt/src/main/resources/static/wsminimal.js
@@ -35,7 +35,7 @@ function connect(){
   var host     = document.location.host;
   var pathname =  document.location.pathname;
   var addr     = "ws://" +host + pathname + "socket"  ;
-    alert("connect addr=" + addr   );
+    console.log("connect addr=" + addr   );
   // Assicura che sia aperta un unica connessione
   if(socket!==undefined && socket.readyState!==WebSocket.CLOSED){
     alert("WARNING: Connessione WebSocket già stabilita");
@@ -43,7 +43,7 @@ function connect(){
   var socket = new WebSocket(addr); //CONNESSIONE
 
   socket.onopen = function (event) {
-    alert("connected")
+    console.log("connected")
     //addMessageToWindow("Connected");
   };
   socket.onmessage = function (event) { //RICEZIONE
